4435e30afde8ca70f622eaaac9128a209ec87955
hzheng <hzheng@expedia.com>
2018-07-10 12:58:31 -0700

    Check in petcare vendor
    Check in petcare vendor and change main ui to tabs to host multiple vendor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { AppComponent } from './app.component';
 import { SeafoodbuffetComponent } from './seafoodbuffet/seafoodbuffet.component';
 import { PetcareComponent } from './petcare/petcare.component';
 import { ProductsService } from './products.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, SeafoodbuffetComponent, PetcareComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'seafoodbuffet', component: SeafoodbuffetComponent},
+      {path: 'petcare', component: PetcareComponent}
+      ]
+    )
+  ],
   providers: [ProductsService],
   bootstrap: [AppComponent]
 })
