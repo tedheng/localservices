4904c4c894b4d4706c3e4cb6e4c95706a89bfcd5
Ted Heng <theng@expedia.com>
2018-07-10 17:48:27 -0700

    fixed some issues

--- src/app/orders.service.ts
+++ src/app/orders.service.ts
@@ -6,6 +6,6 @@ export class OrdersService {
   constructor(private http: HttpClient) { }
 
   getOrders(inputVendorId: number) {
-    return this.http.get('http://belmj06jhgq.sea.corp.expecn.com:3333/orders?vendorid=inputVendorId');
+    return this.http.get('http://belmj06jhgq.sea.corp.expecn.com:3333/orders?vendorid=' + inputVendorId);
   }
 }
