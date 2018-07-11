eabbcb1ebb1f049af1b7d2e399e7a9a382ce9a51
Mark Kramer <mkramer@expedia.com>
2018-07-11 10:41:10 -0700

    User Observables with the async pipe

--- src/app/seafoodbuffet/seafoodbuffet.component.ts
+++ src/app/seafoodbuffet/seafoodbuffet.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { OrdersService } from '../orders.service';
 import { VendorsService } from '../vendors.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-seafoodbuffet',
@@ -11,30 +12,19 @@ import { VendorsService } from '../vendors.service';
 export class SeafoodBuffetComponent implements OnInit {
   vendorId: Number = 100;
   headerTitle: String = 'Seafood Buffet';
-  products: any;
-  orders: any;
-  vendor: any;
+  products$: Observable<any>;
+  orders$: Observable<any>;
+  vendor$: Observable<any>;
+
   constructor(
     private productsService: ProductsService,
     private ordersService: OrdersService,
-    private vendorsService: VendorsService) {}
+    private vendorsService: VendorsService
+  ) {}
 
   ngOnInit() {
-    this.vendor = this.vendorsService.getVendor(this.vendorId);
-    this.products = this.productsService.getProducts(this.vendorId);
-    this.orders = this.ordersService.getOrders(this.vendorId);
-
-    this.productsService
-      .getProducts(this.vendorId)
-      .subscribe(
-        val => this.products = val);
-    this.ordersService
-      .getOrders(this.vendorId)
-      .subscribe(
-        val => this.orders = val);
-    this.vendorsService
-      .getVendor(this.vendorId)
-      .subscribe(
-        val => this.vendor = val);
+    this.vendor$ = this.vendorsService.getVendor(this.vendorId);
+    this.products$ = this.productsService.getProducts(this.vendorId);
+    this.orders$ = this.ordersService.getOrders(this.vendorId);
   }
 }
